1c494aab1717ff8f3b1e2c08f4b576f97df42d7b
igorabreu29 <abreusense@gmail.com>
2026-05-21 12:11:33 -0300

    feat: add Fastify plugin to seed request context store

---
+++ src/infra/http/plugins/request-context-plugin.ts
@@ -0,0 +1,13 @@
+import type { FastifyPluginCallback } from "fastify";
+import fp from "fastify-plugin";
+import { requestContext } from "../logger/request-context.ts";
+
+const requestContextPlugin: FastifyPluginCallback = (app, _, done) => {
+  app.addHook("onRequest", (req, _, next) => {
+    requestContext.run({ requestId: String(req.id) }, next);
+  });
+
+  done();
+};
+
+export default fp(requestContextPlugin);

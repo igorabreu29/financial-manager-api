41f2772bb9b43b4b39e4a48cc4c9f8d116b75a53
igorabreu29 <abreusense@gmail.com>
2026-05-23 22:47:34 -0300

    feat: map InvalidRefreshTokenError to UnauthorizedError in dispatch-error

--- src/infra/http/errors/dispatch-error.ts
+++ src/infra/http/errors/dispatch-error.ts
@@ -1,14 +1,20 @@
 import { ResourceAlreadyExistsError } from "@/core/errors/resource-already-exists-error.ts";
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error.ts";
 import { InvalidCodeError } from "@/domain/accounts/app/use-cases/errors/invalid-code-error.ts";
+import { InvalidRefreshTokenError } from "@/domain/accounts/app/use-cases/errors/invalid-refresh-token-error.ts";
 import { PasswordsDontMatch } from "@/domain/accounts/app/use-cases/errors/passwords-dont-match.ts";
 import { WrongCredentialsError } from "@/domain/accounts/app/use-cases/errors/wrong-credentials-error.ts";
 import { InvalidCategoryError } from "@/domain/transactions/enterprise/entities/errors/invalid-category-error.ts";
 import { BadRequestError } from "./bad-request-error.ts";
 import { ConflictError } from "./conflict-error.ts";
+import { UnauthorizedError } from "./unauthorized-error.ts";
 
 export function dispatchError(error: Error) {
 	const errorClassName = error.constructor.name;
+
+	if ([InvalidRefreshTokenError.name].includes(errorClassName)) {
+		return new UnauthorizedError(error.message);
+	}
 
 	if (
 		[
